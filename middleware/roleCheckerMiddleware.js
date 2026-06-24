0a927ae72fe6fda14597b49b42dfc7e97c5fa00d
Aashu Goswami <goswamiji505@gmail.com>
2026-06-24 21:51:55 +0530

    Updated middleware realted to authorization and authentication

---
+++ middleware/roleCheckerMiddleware.js
@@ -0,0 +1,29 @@
+exports.superAdminOnly = (req, res, next) => {
+  if (req.user.role !== 'superAdmin') {
+    return res.status(403).json({ 
+        success : false,
+        message : 'Access denied. Super admin only.' 
+    });
+  }
+  next();
+};
+
+exports.instAdminOnly = (req, res, next) => {
+  if (req.user.role !== 'instAdmin') {
+    return res.status(403).json({ 
+        success : false,
+        message : 'Access denied. Institution admin only.' 
+    });
+  }
+  next();
+};
+
+exports.studentOnly = (req, res, next) => {
+  if (req.user.role !== 'student') {
+    return res.status(403).json({
+        success : false,
+        message : 'Access denied. Students only.' 
+    });
+  }
+  next();
+};
