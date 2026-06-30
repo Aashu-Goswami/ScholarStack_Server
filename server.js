f1844d573e7672e0e6b9b664459b0c1e58c4bcd2
Dhriti Jain <dhritirjain@gmail.com>
2026-06-30 18:47:25 +0530

    Update server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+// LOAD ENV VARIABLES
+dotenv.config();
+
+// CONNECT TO DATABASE
+connectDB();
+
+const app = express();
+
+// MIDDLEWARE TO PARSE JSON
+app.use(express.json());
+
+// TEST ROUTE
+app.get('/', (req, res) => {
+    res.send('ScholarStack API is running...');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
